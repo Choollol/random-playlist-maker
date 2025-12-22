5870d4d1ef3f704a1ededcf017cc7331994e84e2
Choollol <67297627+Choollol@users.noreply.github.com>
2025-12-21 22:28:31 -0800

    Rename auth-client.ts to authClient.ts

--- src/hooks/useAuthUserData.ts
+++ src/hooks/useAuthUserData.ts
@@ -1,4 +1,4 @@
-import { authClient } from "@/lib/auth-client";
+import { authClient } from "@/lib/authClient";
 
 /**
  * @returns The logged-in user's data.
--- src/hooks/useIsSignedIn.ts
+++ src/hooks/useIsSignedIn.ts
@@ -1,4 +1,4 @@
-import { authClient } from "@/lib/auth-client";
+import { authClient } from "@/lib/authClient";
 
 /**
  * @returns Whether the user is logged in and authenticated.
