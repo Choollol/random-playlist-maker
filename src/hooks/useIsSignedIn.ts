b4724442085dc2578b113a5afb633fe28500c3d2
Choollol <67297627+Choollol@users.noreply.github.com>
2025-12-21 20:44:37 -0800

    Rename useIsLoggedIn to useIsSignedIn

--- src/hooks/useIsSignedIn.ts
+++ src/hooks/useIsSignedIn.ts
@@ -3,9 +3,9 @@ import { authClient } from "@/lib/auth-client";
 /**
  * @returns Whether the user is logged in and authenticated.
  */
-const isLoggedIn = () => {
+const useIsSignedIn = (): boolean => {
   const session = authClient.useSession();
   return session.data !== null;
 };
 
-export default isLoggedIn;
+export default useIsSignedIn;
