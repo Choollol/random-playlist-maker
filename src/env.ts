868faee2c95e0590f53ff28ebfc19a77dc5536b7
Choollol <67297627+Choollol@users.noreply.github.com>
2025-12-21 20:14:01 -0800

    Add Google API key to env

--- src/env.ts
+++ src/env.ts
@@ -7,6 +7,7 @@ loadEnvConfig(projectDir);
 const envSchema = z.object({
   BETTER_AUTH_SECRET: z.string(),
   BETTER_AUTH_URL: z.string(),
+  GOOGLE_API_KEY: z.string(),
   GOOGLE_CLIENT_ID: z.string(),
   GOOGLE_CLIENT_SECRET: z.string(),
 });
