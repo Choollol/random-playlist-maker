6a99f45eaf848a79f1ce5ed693edb38f839106d1
Choollol <67297627+Choollol@users.noreply.github.com>
2026-01-13 16:03:40 -0800

    Move gapi initialization tracking to Initializer.tsx

--- src/components/Initializer.tsx
+++ src/components/Initializer.tsx
@@ -1,22 +1,41 @@
+import useIsSignedIn from "@/hooks/useIsSignedIn";
 import { initDB } from "@/lib/db";
+import { initGapiClient } from "@/lib/utils/gapiUtils";
 import { useInitializationStateStore } from "@/store/useInitializationStateStore";
 import { useEffect } from "react";
 import { useShallow } from "zustand/react/shallow";
 
-const Initializer = () => {
+interface Props {
+  isGapiLoaded: boolean;
+}
+
+const Initializer = ({ isGapiLoaded }: Props) => {
+  const isSignedIn = useIsSignedIn();
+
   const {
     isGapiInitialized,
     isDatabaseInitialized,
+    setGapiInitialized,
     setDatabaseInitialized,
     setEverythingInitialized,
   } = useInitializationStateStore(
     useShallow((state) => ({
       isGapiInitialized: state.isGapiInitialized,
       isDatabaseInitialized: state.isDatabaseInitialized,
+      setGapiInitialized: state.setGapiInitialized,
       setDatabaseInitialized: state.setDatabaseInitialized,
       setEverythingInitialized: state.setEverythingInitialized,
     }))
   );
+
+  useEffect(() => {
+    if (isSignedIn && isGapiLoaded) {
+      (async function () {
+        await initGapiClient();
+        setGapiInitialized();
+      })();
+    }
+  }, [isSignedIn, isGapiLoaded, setGapiInitialized]);
 
   useEffect(() => {
     (async () => {
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,32 +1,14 @@
 import Initializer from "@/components/Initializer";
-import useIsSignedIn from "@/hooks/useIsSignedIn";
 import { defaultStyles, theme } from "@/lib/styling/defaultStyling";
-import { initGapiClient } from "@/lib/utils/gapiUtils";
-import { useInitializationStateStore } from "@/store/useInitializationStateStore";
 import { CssBaseline, GlobalStyles, ThemeProvider } from "@mui/material";
 import type { AppProps } from "next/app";
 import Script from "next/script";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 const globalStyles = <GlobalStyles styles={defaultStyles} />;
 
 export default function App({ Component, pageProps }: AppProps) {
-  const isSignedIn = useIsSignedIn();
-
-  const setGapiInitialized = useInitializationStateStore(
-    (state) => state.setGapiInitialized
-  );
-
   const [isGapiLoaded, setIsGapiLoaded] = useState(false);
-
-  useEffect(() => {
-    if (isSignedIn && isGapiLoaded) {
-      (async function () {
-        await initGapiClient();
-        setGapiInitialized();
-      })();
-    }
-  }, [isSignedIn, isGapiLoaded, setGapiInitialized]);
 
   const handleGapiLoad = () => {
     gapi.load("client", () => {
@@ -41,7 +23,7 @@ export default function App({ Component, pageProps }: AppProps) {
 
       <Script src="https://apis.google.com/js/api.js" onLoad={handleGapiLoad} />
 
-      <Initializer />
+      <Initializer isGapiLoaded={isGapiLoaded} />
 
       <Component {...pageProps} />
     </ThemeProvider>
