33584a443e72609fdc4b30c08690b17f032497c3
Choollol <67297627+Choollol@users.noreply.github.com>
2026-02-14 16:22:08 -0800

    Give user profile button popover with sign out

--- src/components/UserProfileButton.tsx
+++ src/components/UserProfileButton.tsx
@@ -1,18 +1,69 @@
 import UserProfilePicture from "@/components/UserProfilePicture";
 import { authClient } from "@/lib/authClient";
-import { Button } from "@mui/material";
+import { createStyles } from "@/lib/styling/styling";
+import { Logout } from "@mui/icons-material";
+import {
+  IconButton,
+  ListItemIcon,
+  ListItemText,
+  MenuItem,
+  Popover,
+} from "@mui/material";
+import { useState } from "react";
+
+const styles = createStyles({
+  popover: {
+    padding: "4px",
+  },
+});
 
 /**
  * Shown when user _is_ signed in.
  */
 const UserProfileButton = () => {
-  const handleClick = () => {
+  const [doOpenPopver, setDoOpenPopover] = useState(false);
+  const [anchorEl, setAnchorEl] = useState<HTMLButtonElement | null>(null);
+
+  const handleAnchorButtonClick = (
+    event: React.MouseEvent<HTMLButtonElement>,
+  ) => {
+    setAnchorEl(event.currentTarget);
+    setDoOpenPopover(true);
+  };
+
+  const handlePopoverClose = () => {
+    setDoOpenPopover(false);
+  };
+
+  const handleSignOutClick = () => {
     authClient.signOut();
   };
+
   return (
-    <Button onClick={handleClick}>
-      <UserProfilePicture />
-    </Button>
+    <>
+      <IconButton onClick={handleAnchorButtonClick}>
+        <UserProfilePicture />
+      </IconButton>
+
+      <Popover
+        open={doOpenPopver}
+        onClose={handlePopoverClose}
+        anchorEl={anchorEl}
+        anchorOrigin={{ vertical: "bottom", horizontal: "left" }}
+        slotProps={{
+          paper: {
+            sx: styles.popover,
+          },
+        }}
+      >
+        <MenuItem onClick={handleSignOutClick}>
+          <ListItemIcon>
+            <Logout />
+          </ListItemIcon>
+          <ListItemText>Sign Out</ListItemText>
+        </MenuItem>
+      </Popover>
+    </>
   );
 };
 
--- src/components/UserProfilePicture.tsx
+++ src/components/UserProfilePicture.tsx
@@ -1,14 +1,23 @@
 import useAuthUserData from "@/hooks/useAuthUserData";
 import { isDefined } from "@/lib/utils/typeUtils";
 import Image from "next/image";
+import { AccountCircle } from "@mui/icons-material";
+
+const IMAGE_WIDTH = 32;
 
 const UserProfilePicture = () => {
   const userData = useAuthUserData();
 
   return isDefined(userData?.image) ? (
-    <Image src={userData.image} alt="User's profile picture" fill={true} />
+    <Image
+      src={userData.image}
+      alt="User's profile picture"
+      width={IMAGE_WIDTH}
+      height={IMAGE_WIDTH}
+      style={{ borderRadius: "100%" }}
+    />
   ) : (
-    <div>hi</div>
+    <AccountCircle />
   );
 };
 
