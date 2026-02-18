1c6adaa92dd3d29e78ffa449f592847f7551e226
Choollol <67297627+Choollol@users.noreply.github.com>
2026-02-17 18:34:53 -0800

    Make bug report button direct link

--- src/components/header/BugReportButton.tsx
+++ src/components/header/BugReportButton.tsx
@@ -1,7 +1,7 @@
 import { createStyleGroup } from "@/lib/styling/styling";
 import { BUG_REPORT_URL } from "@/lib/utils/miscUtils";
 import { BugReport } from "@mui/icons-material";
-import { IconButton, Link, Popover, Typography } from "@mui/material";
+import { IconButton, Link, Popover, Tooltip, Typography } from "@mui/material";
 import { useState } from "react";
 
 const styles = createStyleGroup({
@@ -12,39 +12,14 @@ const styles = createStyleGroup({
 });
 
 const BugReportButton = () => {
-  const [doOpenPopver, setDoOpenPopover] = useState(false);
-  const [anchorEl, setAnchorEl] = useState<HTMLButtonElement | null>(null);
-
-  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-    setAnchorEl(event.currentTarget);
-    setDoOpenPopover(true);
-  };
-
-  const handlePopoverClose = () => {
-    setDoOpenPopover(false);
-  };
-
   return (
-    <div>
-      <IconButton size="large" onClick={handleClick}>
-        <BugReport />
-      </IconButton>
-
-      <Popover
-        open={doOpenPopver}
-        onClose={handlePopoverClose}
-        anchorEl={anchorEl}
-        anchorOrigin={{ vertical: "bottom", horizontal: "left" }}
-        slotProps={{
-          paper: {
-            sx: styles.popover,
-          },
-        }}
-      >
-        <Typography>Something went wrong?</Typography>
-        <Link href={BUG_REPORT_URL}>Submit a bug report!</Link>
-      </Popover>
-    </div>
+    <Tooltip title="Report a bug!">
+      <Link href={BUG_REPORT_URL}>
+        <IconButton size="large">
+          <BugReport />
+        </IconButton>
+      </Link>
+    </Tooltip>
   );
 };
 
