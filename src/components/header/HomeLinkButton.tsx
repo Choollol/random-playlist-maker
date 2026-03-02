24fd3f7eab1aabd8791c674851644e11239c7549
Choollol <67297627+Choollol@users.noreply.github.com>
2026-03-01 16:06:16 -0800

    Add tooltip and fix home button

--- src/components/header/HomeLinkButton.tsx
+++ src/components/header/HomeLinkButton.tsx
@@ -1,21 +1,37 @@
 "use client";
 
-import { IconButton } from "@mui/material";
+import { IconButton, Tooltip } from "@mui/material";
 import Link from "next/link";
 import Image from "next/image";
+import { createStyleGroup } from "@/lib/styling/styling";
 
-const LOGO_WIDTH = 40;
+const LOGO_WIDTH = 36;
+
+const styles = createStyleGroup({
+  button: {
+    margin: 0,
+    padding: 1,
+  },
+});
 
 const HomeLinkButton = () => {
   return (
-    <IconButton href="/" LinkComponent={Link} size="large" disableRipple>
-      <Image
-        src="/logo.png"
-        width={LOGO_WIDTH}
-        height={LOGO_WIDTH}
-        alt="Home page button"
-      />
-    </IconButton>
+    <Tooltip title="Home">
+      <IconButton
+        href="/"
+        LinkComponent={Link}
+        size="large"
+        disableRipple
+        sx={styles.button}
+      >
+        <Image
+          src="/logo.png"
+          width={LOGO_WIDTH}
+          height={LOGO_WIDTH}
+          alt="Home page button"
+        />
+      </IconButton>
+    </Tooltip>
   );
 };
 
