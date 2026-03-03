c27e28d4a1f8033dd6632ca1486dc8c1ed362bfd
Choollol <67297627+Choollol@users.noreply.github.com>
2026-03-02 20:45:09 -0800

    Add loading indicator to sign in button

--- src/components/SignInButton.tsx
+++ src/components/SignInButton.tsx
@@ -2,14 +2,22 @@
 
 import { signInGoogle } from "@/lib/authClient";
 import { Button, Tooltip, Typography } from "@mui/material";
+import { useState } from "react";
 
 /**
  * Shown when user is _not_ signed in.
  */
 const SignInButton = () => {
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleClick = () => {
+    setIsLoading(true);
+    signInGoogle();
+  };
+
   return (
     <Tooltip title="Sign in with Google">
-      <Button onClick={signInGoogle}>
+      <Button onClick={handleClick} loading={isLoading}>
         <Typography variant="h6">Sign in</Typography>
       </Button>
     </Tooltip>
