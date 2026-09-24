1ffab77669b9f5b030af14415877ad9958d93121
Choollol <67297627+Choollol@users.noreply.github.com>
2026-09-24 11:28:47 -0700

    Fix video count field state mismatch

--- src/components/CreatePlaylistForm.tsx
+++ src/components/CreatePlaylistForm.tsx
@@ -160,7 +160,7 @@ const CreatePlaylistForm = () => {
               setValue("numPlaylistItems", value ?? DEFAULT_VIDEO_COUNT)
             }
             label={`Number of videos (${MIN_VIDEO_COUNT}-${MAX_VIDEO_COUNT})`}
-            defaultValue={DEFAULT_VIDEO_COUNT}
+            value={formData.numPlaylistItems ?? DEFAULT_VIDEO_COUNT}
             min={MIN_VIDEO_COUNT}
             max={MAX_VIDEO_COUNT}
             error={!!errors.numPlaylistItems}
