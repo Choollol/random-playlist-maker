699e905ad07887b82f324a23d6a50edfb02f6b9f
Choollol <67297627+Choollol@users.noreply.github.com>
2026-09-25 10:01:49 -0700

    Reapply "Add temp console logs"
    This reverts commit c1d5b3f7d49076fb18f0237b07a7aafd00b9c3e2.

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -46,6 +46,7 @@ function getDbApp() {
  */
 export async function saveToDatabase(collectionKey: string, documentKey: string, data: object) {
   const app = getDbApp();
+  console.log(">>", getAuth(app).currentUser);
   setDoc(doc(getFirestore(app), collectionKey, documentKey), {
     ...data,
     // This has to match the firestore rule
@@ -76,6 +77,7 @@ export async function signInToDatabase(uid: string) {
       }
     });
   });
+  console.log(">", getAuth(app).currentUser);
 }
 
 export async function signOutOfDatabase() {
